52250e8119c61e44c492e9022de453e4adf8d28e
Jason Smith <jason@innovationinsoftware.com>
2020-07-15 15:14:30 -0700

    initial

---
+++ variables.tf
@@ -0,0 +1,91 @@
+##Amazon Infrastructure
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+##Create k8s security group
+resource "aws_security_group" "k8s_sg" {
+  name        = "k8s_sg"
+  vpc_id            = "${var.aws_vpc_id}"
+  description = "Allow all inbound traffic necessary for k8s"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+  tags {
+    Name = "k8s_sg"
+  }
+}
+
+##Find latest Ubuntu 16.04 AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+##Create k8s Master Instance
+resource "aws_instance" "aws-k8s-master" {
+  subnet_id              = "${var.aws_subnet_id}"
+  depends_on             = ["aws_security_group.k8s_sg"]
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  instance_type          = "${var.aws_instance_size}"
+  vpc_security_group_ids = ["${aws_security_group.k8s_sg.id}"]
+  key_name               = "${var.aws_key_name}"
+  count                  = "${var.aws_master_count}"
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 20
+    delete_on_termination = true
+  }
+  tags {
+    Name = "k8s-master-${count.index}"
+    role = "k8s-master"
+  }
+}
+
+##Create AWS k8s Workers
+resource "aws_instance" "k8s-members" {
+  subnet_id              = "${var.aws_subnet_id}"
+  depends_on             = ["aws_security_group.k8s_sg"]
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  instance_type          = "${var.aws_instance_size}"
+  vpc_security_group_ids = ["${aws_security_group.k8s_sg.id}"]
+  key_name               = "${var.aws_key_name}"
+  count                  = "${var.aws_worker_count}"
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 20
+    delete_on_termination = true
+  }
+  tags {
+    Name = "k8s-member-${count.index}"
+    role = "k8s-member"
+  }
+}
+
+
+output "k8s-master public_ips" {
+  value = ["${aws_instance.aws-k8s-master.*.public_ip}"]
+}
+
+output "k8s-node public_ips" {
+  value = ["${aws_instance.k8s-members.*.public_ip}"]
+}
